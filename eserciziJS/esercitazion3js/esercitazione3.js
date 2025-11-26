4eac6b8a153f65d844dd20f01e94035798bb9c59
mino-bit <colonna.davide.01@gmail.com>
2025-11-26 14:01:02 +0100

    fine di questa esercitazione

--- eserciziJS/esercitazion3js/esercitazione3.js
+++ eserciziJS/esercitazion3js/esercitazione3.js
@@ -6,38 +6,73 @@ for (let i = 0; i < 3; i++) {
     }else{
          if (i==1){
         mese= window.prompt("inserisci il mese");
-        
-    }else{
-      
-        anno= window.prompt("inserisci l'anno");
-    }
-    }
-    
-  
-}
+            switch(mese){
+                case "1":
+                    mese="Gennaio";
+                break;
+                case "2":
+                    mese="Febbraio";
+                break;
+                case "3":
+                    mese="Marzo";
+                break;
+                case "4":
+                    mese="Aprile";
+                break;
+                case "5":
+                    mese="Maggio";
+                break;
+                case "6":
+                    mese="Giugno";
+                break;
+                case "7":
+                    mese="Luglio";
+                break;
+                case "8":
+                    mese="Agosto";
+                break;
+                case "9":
+                    mese="Settembre";
+                break;
+                case "10":
+                    mese="Ottobre";
+                break;
+                case "11":
+                    mese="Novembre";
+                break;
+                case "12":
+                    mese="Dicembre";
+                break;
+
+            }               
+                }else{
+                      anno= window.prompt("inserisci l'anno");
+                    }
+         }
+        }
 let dataInserita = new Date(giorno+" / "+mese+" / "+anno);
 let giornoSettimana = dataInserita.getDay();
 switch(giornoSettimana){
+    case 0:
+    console.log("Lunedi' " + giorno + " " + mese+ " " + anno);
+    break;
     case 1:
-    console.log("Lunedì " + giorno + " " + mese);
+    console.log("Martedi' " + giorno + " " + mese+ " " + anno);
     break;
     case 2:
-    console.log("Martedì " + giorno + " " + mese);
+    console.log("Mercoledi' " + giorno + " " + mese+ " " + anno);
     break;
     case 3:
-    console.log("Mercoledì " + giorno + " " + mese);
+    console.log("Giovedi' " + giorno + " " + mese+ " " + anno);
     break;
     case 4:
-    console.log("Giovedì " + giorno + " " + mese);
+    console.log("Venerdi' " + giorno + " " + mese+ " " + anno);
     break;
     case 5:
-    console.log("Venerdì " + giorno + " " + mese);
+    console.log("Sabato " + giorno + " " + mese+ " " + anno);
     break;
     case 6:
-    console.log("Sabato " + giorno + " " + mese);
-    break;
-    case 7:
-    console.log("Domenica " + giorno + " " + mese);
+    console.log("Domenica " + giorno + " " + mese+ " " + anno);
     break;
 
 }
